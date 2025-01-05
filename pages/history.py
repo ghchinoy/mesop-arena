627ca71bb26dd63fb2d0ced1b03b73f9e94f7d0d
Hussain Chinoy <ghchinoy@google.com>
2025-01-05 11:00:10 -0700

    refactor: moves dialog component to components for reuse

--- pages/history.py
+++ pages/history.py
@@ -13,11 +13,12 @@
 # limitations under the License.
 """ History page"""
 import mesop as me
-from typing import Callable
 
 from common.metadata import get_latest_votes
 
 from components.header import header
+from components.dialog import dialog
+
 from components.page_scaffold import (
     page_scaffold,
     page_frame,
@@ -136,50 +137,6 @@ def history_page_content(app_state: me.state):
                             me.html(html=f'With prompt: "<em>{prompt}</em>"')
 
 
-@me.content_component
-def dialog(
-    *, is_open: bool, on_click_background: Callable | None = None
-):  # pylint: disable=not-context-manager
-    """renders a dialog component"""
-    with me.box(
-        style=me.Style(
-            background=(
-                "rgba(0, 0, 0, 0.4)"
-                if me.theme_brightness() == "light"
-                else "rgba(255, 255, 255, 0.4)"
-            ),
-            display="block" if is_open else "none",
-            height="100%",
-            width="100%",
-            overflow_x="auto",
-            overflow_y="auto",
-            position="fixed",
-            z_index=1000,
-        ),
-    ):
-        with me.box(
-            on_click=on_click_background,
-            style=me.Style(
-                place_items="center",
-                display="grid",
-                height="50vh",
-            ),
-        ):
-            with me.box(
-                style=me.Style(
-                    background=me.theme_var("surface-container-lowest"),
-                    border_radius=20,
-                    box_sizing="content-box",
-                    box_shadow=(
-                        "0 3px 1px -2px #0003, 0 2px 2px #00000024, 0 1px 5px #0000001f"
-                    ),
-                    margin=me.Margin.symmetric(vertical="0", horizontal="auto"),
-                    padding=me.Padding.all(20),
-                )
-            ):
-                me.slot()
-
-
 def on_click_image_dialog(e: me.ClickEvent):
     """show larger image"""
     page_state = me.state(PageState)
@@ -188,7 +145,7 @@ def on_click_image_dialog(e: me.ClickEvent):
 
 
 def on_click_background_close(e: me.ClickEvent):
-    """close dialog"""
+    """implementation dialog component's close event """
     page_state = me.state(PageState)
     page_state.is_open = False
 
