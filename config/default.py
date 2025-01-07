e1b2b903044751f943ab099705a9495f04e3fc36
Hussain Chinoy <ghchinoy@google.com>
2025-01-06 21:27:33 -0700

    feat: flux.1 model garden

--- config/default.py
+++ config/default.py
@@ -32,11 +32,12 @@ class Default:
 
     # pylint: disable=invalid-name
     PROJECT_ID: str = os.environ.get("PROJECT_ID")
+    PROJECT_NUMBER: str = os.environ.get("PROJECT_NUMBER")
     LOCATION: str = os.environ.get("LOCATION", "us-central1")
     MODEL_ID: str = os.environ.get("MODEL_ID", "gemini-2.0-flash-exp")
     INIT_VERTEX: bool = True
 
-    GENMEDIA_BUCKET = os.environ.get("GENMEDIA_BUCKET")
+    GENMEDIA_BUCKET = os.environ.get("GENMEDIA_BUCKET", f"{PROJECT_ID}-genmedia")
     IMAGE_COLLECTION_NAME = os.environ.get("IMAGE_COLLECTION_NAME", "arena_images")
     IMAGE_RATINGS_COLLECTION_NAME = os.environ.get("IMAGE_RATINGS_COLLECTION_NAME", "arena_elo")
     ELO_K_FACTOR = os.environ.get("ELO_K_FACTOR", 32)
@@ -45,5 +46,10 @@ class Default:
     MODEL_IMAGEN2 = "imagegeneration@006"
     MODEL_IMAGEN3_FAST = "imagen-3.0-fast-generate-001"
     MODEL_IMAGEN3 = "imagen-3.0-generate-001"
+    
+    # model garden image models
+    MODEL_FLUX1 = "black-forest-labs/FLUX.1-schnell"
+    MODEL_FLUX1_ENDPOINT = "9021247714809085952" #"flux1-schnell-1736198391493@1"
+
 
     # pylint: disable=invalid-name
