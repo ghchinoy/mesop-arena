3fa6158dc1dd2d3e52cc1a64be9f281d708fef41
Emmanuel Awa <3170798+awaemmanuel@users.noreply.github.com>
2025-04-01 14:48:42 -0400

    Remove: Address review comments (#27)

--- config/default.py
+++ config/default.py
@@ -37,7 +37,6 @@ class Default:
     INIT_VERTEX: bool = True
 
     GENMEDIA_BUCKET = os.environ.get("GENMEDIA_BUCKET")
-    GENMEDIA_GENERATED_BUCKET = os.environ.get("GENMEDIA_GENERATED_BUCKET")
     IMAGE_FIREBASE_DB = os.environ.get("IMAGE_FIREBASE_DB", "")
     IMAGE_COLLECTION_NAME = os.environ.get("IMAGE_COLLECTION_NAME", "arena_images")
     STUDY_COLLECTION_NAME = os.environ.get("STUDY_COLLECTION_NAME", "arena_study")
@@ -63,9 +62,6 @@ class Default:
         if not self.PROJECT_ID:
             raise ValueError("PROJECT_ID environment variable is not set.")
         
-        if not self.GENMEDIA_GENERATED_BUCKET:
-            raise ValueError("GENMEDIA_GENERATED_BUCKET environment variable is not set.")
-
         if not self.GENMEDIA_BUCKET:
             raise ValueError("GENMEDIA_BUCKET environment variable is not set.")
 
