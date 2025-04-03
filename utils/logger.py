87227c8559a2c0da73ba35bb8a6e61974786e3e6
Emmanuel Awa <emmanuelawa@google.com>
2025-04-03 22:55:17 +0000

    Add: Improve README for Spanner and benefits for Study. Little updates for settings page

--- utils/logger.py
+++ utils/logger.py
@@ -11,40 +11,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Logger configuration for GenMedia Arena """
-from curses import ERR
+"""Logger configuration for GenMedia Arena"""
 import logging
-from termios import VERASE
+
 
 class LogLevel:
     """Enum for log levels"""
+
     OFF = 0
     ON = 1
     WARNING = 2
     ERROR = 3
-    _names = {
-        0: "OFF",
-        1: "ON",
-        2: "WARNING",
-        3: "ERROR"
-    }
+    _names = {0: "OFF", 1: "ON", 2: "WARNING", 3: "ERROR"}
 
     def __repr__(self):
         return self._names.get(self, "UNKNOWN")
+
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
-    datefmt="%H:%M:%S",)
+    datefmt="%H:%M:%S",
+)
+
 
 def log(message: str, level: LogLevel = LogLevel.ON):
     """Log a message at the specified level."""
+    log_methods = {
+        LogLevel.ON: logging.info,
+        LogLevel.WARNING: logging.warning,
+        LogLevel.ERROR: logging.error,
+    }
+
     if level == LogLevel.OFF:
         return  # Do not log if level is OFF
-    if level == LogLevel.ON:
-        logging.info(message)
-    elif level == LogLevel.WARNING:
-        logging.warning(message)
-    elif level == LogLevel.ERROR:
-        logging.error(message)
+
+    log_method = log_methods.get(level)
+    if log_method:
+        log_method(message)
     else:
-        raise ValueError("Invalid log level specified. Use LogLevel.ON, LogLevel.OFF, LogLevel.WARNING, or LogLevel.ERROR.")
+        raise ValueError(
+            f"Invalid log level specified: {level}. Use LogLevel.ON, LogLevel.OFF, LogLevel.WARNING, or LogLevel.ERROR."
+        )
